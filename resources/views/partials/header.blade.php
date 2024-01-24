220433d4acc6de5ba6060539c9bf80ab62287b1d
EnricoDG97 <enrico.delgaudio97@gmail.com>
2024-01-24 18:14:12 +0100

    feat: added controller and navbar

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,5 +1,14 @@
 <header>
 
-    <h2>header</h2>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid d-flex justify-content-space-between">
+          <a class="navbar-brand" href="#">DC COMICS</a>
+          <div id="navbarText">
+            <span class="navbar-text">
+              Comics available
+            </span>
+          </div>
+        </div>
+      </nav>
 
 </header>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="container mt-5">
     <h1>Welcome to laravel</h1>
+</div>
 @endsection
